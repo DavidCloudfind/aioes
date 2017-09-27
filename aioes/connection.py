d498e9aad095ca9cbb6c7b67f3a2f939a052616a
DavidCloudfind <dmercer@cloudfindhq.com>
2017-09-27 13:53:21 +0100

    syntax update

--- aioes/connection.py
+++ aioes/connection.py
@@ -19,13 +19,12 @@ class Connection:
 
     def __init__(self, endpoint, *, loop, verify_ssl=True, connector=None):
         self._endpoint = endpoint
-        self._session = aiohttp.ClientSession(
-            # limit number of connections?
-            connector=connector or aiohttp.TCPConnector(
-                use_dns_cache=True,
-                loop=loop,
-                verify_ssl=verify_ssl),
-            loop=loop, headers={'Content-Type': 'application/x-ndjson'})
+        self._session = aiohttp.ClientSession(headers={'Content-Type': 'application/x-ndjson'},
+                                              connector=connector or aiohttp.TCPConnector(
+                                              use_dns_cache=True,
+                                              loop=loop,
+                                              verify_ssl=verify_ssl),
+                                              loop=loop)
         self._base_url = yarl.URL('{0.scheme}://{0.host}:{0.port}/'
                                   .format(endpoint))
 
